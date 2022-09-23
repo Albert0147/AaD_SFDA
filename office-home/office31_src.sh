3502416ac2b3513dc452e77ee763516f779a2059
Shiqi Yang <shiqi.yang147@outlook.com>
2022-09-23 11:36:39 +0200

    update

---
+++ office-home/office31_src.sh
@@ -0,0 +1,11 @@
+python office31_src.py --dset a2d  
+
+python office31_src.py --dset a2w  
+
+python office31_src.py --dset d2a 
+
+python office31_src.py --dset d2w  
+
+python office31_src.py --dset w2a  
+
+python office31_src.py --dset w2d  
---
+++ office-home/office31_tar.sh
@@ -0,0 +1,11 @@
+python office31_tar.py --dset a2d  --K 3 --beta 1  --file k3b1
+
+python office31_tar.py --dset w2a  --K 3  --beta 1  --file k3b1
+
+python office31_tar.py --dset a2w  --K 3   --beta 1  --file k3b1
+
+python office31_tar.py --dset d2w  --K 3   --beta 1  --file k3b1
+
+python office31_tar.py --dset d2a --K 3 --beta 1  --file k3b1
+
+python office31_tar.py --dset w2d  --K 3   --beta 1  --file k3b1
