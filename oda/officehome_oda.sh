83396cddc2d8259b0fc63d47bcc5cc6d4f344b75
Shiqi Yang <shiqi.yang147@outlook.com>
2022-12-03 11:21:25 +0100

    upload oda code

---
+++ oda/officehome_oda.sh
@@ -0,0 +1,7 @@
+python tar_open.py --cls_par 0.3 --da oda --dset office-home --gpu_id 1 --s 0 --output_src ckps/source/ --output target_b1/ --beta 1
+
+python tar_open.py --cls_par 0.3 --da oda --dset office-home --gpu_id 1 --s 1 --output_src ckps/source/ --output target_b1/ --beta 1
+
+python tar_open.py --cls_par 0.3 --da oda --dset office-home --gpu_id 1 --s 2 --output_src ckps/source/ --output target_b1/ --beta 1
+
+python tar_open.py --cls_par 0.3 --da oda --dset office-home --gpu_id 1 --s 3 --output_src ckps/source/ --output target_b1/ --beta 1
